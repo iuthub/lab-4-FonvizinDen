ff836655378c6b9e501f86ebd099ddef36f0c885
Lim Denis <fonvizinden39@gmail.com>
2019-03-10 17:35:27 +0500

    Update music.php

--- webpage/music.php
+++ webpage/music.php
@@ -5,6 +5,7 @@
 		<title>Music Viewer</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 		<link href="viewer.css" type="text/css" rel="stylesheet" />
+
 	</head>
 	<body>
 		<div id="header">
@@ -15,28 +16,60 @@
 
 
 		<div id="listarea">
-			<ul id="musiclist">
-				<?php
-				$files = glob("songs/*.mp3");
-				foreach($files as $filename) {
-				?>
-					<li class="mp3item">
-						<a href= "<? =$filemame?>"><?php echo $filename ?></a>
-					</li>
-				<?php
-				}
-				?>
+					<ul id="musiclist">
+				<?php 
+				function musiclist(){
+					if (!isset($_REQUEST['playlist'])){
+						$files=glob("songs/*.mp3");
+						$valid=false;
+					}
+					else{
+						$files = file('songs/' . $_REQUEST['playlist'], FILE_IGNORE_NEW_LINES);
+						$valid=true;
+					}
+					foreach ($files as $file ) {
+						if (!$valid) {
+							$size = filesize($file);
+						  if ($size > 0 && $size < 1024){
+						  	$size = $size . " b";
+						  }
+						  elseif ($size > 1024 && $size < 1048575){
+						  	$size = round($size / 1024, 2) . " kb";
+						  }
+						  elseif ($size > 1048575){
+						  	$size = round($size / 1048575, 2) . " mb";
+						  }
+						}
+						else{
+							$size = filesize('songs/'.$file);
+						  if ($size > 0 && $size < 1024){
+						  	$size = $size . " b";
+						  }
+						  elseif ($size > 1024 && $size < 1048575){
+						  	$size = round($size / 1024, 2) . " kb";
+						  }
+						  elseif ($size > 1048575){
+						  	$size = round($size / 1048575, 2) . " mb";
+						  }
+						}
+						?>
+						 <li class="mp3item"><a href="songs/<?= basename($file) ?>">
+		  	<?= basename($file). " (" . $size . " )" ;?></a></li>
 
-				<?php
-				$Tfiles = glob("songs/*.txt");
-				foreach($Tfiles as $Tfilename) {
-				?>
-					<li class="playlistitem">
-						<a href= "<? =$Tfilemame?>"><?php echo $Tfilename ?></a>
-					</li>
-				<?php
+						<?php 
+					}
 				}
-				?>
+				function showlist(){
+					$playlists=glob("songs/*.txt");
+					foreach($playlists as $playlist){
+				 		?>
+				 		<li class="playlistitem"><a href="music.php?playlist=<?=basename($playlist)?>"><?= basename($playlist);?></a></li>
+				 			<?php } 
+				 		}
+				 		musiclist();
+				 		if(!isset($_REQUEST['playlist']))
+	  					 showlist();
+				 	?>
 			</ul>
 		</div>
 	</body>
